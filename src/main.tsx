0547bcab3335e2c4df8678475580145387322da0
skoppula17 <spoorthikoppula@gmail.com>
2025-08-03 16:09:49 -0400

    made some overall changes to it

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// If the file exists at a different path, update the import accordingly, for example:
+import { ThemeProvider } from "./components/theme-provider.tsx"
+// Or, if the file does not exist, create it at src/components/theme-provider.tsx or .tsx
+import App from './App'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
